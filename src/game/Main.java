bde85b1aaccba8f30ab1c3e5216019399baa896c
KannstDuDasAuch1 <toto.haupt@gmail.com>
2022-06-01 12:50:09 +0200

    test von tobias

--- src/game/Main.java
+++ src/game/Main.java
@@ -3,6 +3,7 @@ package game;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
+		System.out.println("Hello von Tobias");
 	}
 	
 	public int add(int a, int b) {
